745217876f9090677b787a01a679e3d3fcf76daf
jlyahh <3211498596@qq.com>
2023-08-18 23:57:02 +0800

    添加Intel编程文档，设置gdt_table，成功进入保护模式

--- source/os.c
+++ source/os.c
@@ -2,3 +2,15 @@
  * 功能：32位代码，完成多任务的运行
  */
 #include "os.h"
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+// 定义GDT表结构
+struct
+{
+    uint16_t limit_l, base_l, basehl_attr, base_limit;
+} gdt_table[256] __attribute__((aligned(8))) = {
+    [KERNEL_COOE_SEG / 8] = {0xffff, 0x0000, 0x9a00, 0x00cf},
+    [KERNEL_DATA_SEG / 8] = {0xffff, 0x0000, 0x9200, 0x00cf},
+};
--- source/os.h
+++ source/os.h
@@ -4,4 +4,7 @@
 #ifndef OS_H
 #define OS_H
 
+#define KERNEL_COOE_SEG 8
+#define KERNEL_DATA_SEG 16
+
 #endif // OS_H
