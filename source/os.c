99e3bbfac384366fb80e3d33256b64eab4c92411
jlyahh <3211498596@qq.com>
2023-09-06 22:51:48 +0800

    配置IDT表，实现中断功能

--- source/os.c
+++ source/os.c
@@ -19,10 +19,17 @@ typedef unsigned int uint32_t;
 #define MAP_ADDR 0x80000000
 
 uint8_t map_phy_buffer[4096] __attribute__((aligned(4096))) = {0x36};
+;
 // 定义页目录表
 uint32_t pg_dir[1024] __attribute__((aligned(4096))) = {
     [0] = (0) | PDE_P | PDE_W | PDE_U | PDE_PS,
 };
+
+// IDT表结构
+struct
+{
+    uint16_t offset_l, selector, attr, offset_h;
+} idt_table[256] __attribute__((aligned(8))) = {1};
 
 // 定义二级页表
 static uint32_t page_table[1024] __attribute__((aligned(4096))) = {PDE_U};
@@ -41,6 +48,7 @@ void outb(uint8_t data, uint16_t port)
     __asm__ __volatile__("outb %[v], %[p]" ::[p] "d"(port), [v] "a"(data));
 }
 
+void timer_int(void);
 void os_init(void)
 {
     // 初始化8259主片从片
@@ -58,10 +66,16 @@ void os_init(void)
 
     // 配置8253
     // 100ms产生一次中断
-    int tmo = 1193180 / 100;
+    // int tmo = 1193180 / 100;
+    int tmo = (1193180);
     outb(0x36, 0x43);
     outb((uint8_t)tmo, 0x40);
     outb(tmo >> 8, 0x40);
+
+    idt_table[0x20].offset_l = (uint32_t)timer_int & 0xFFFF;
+    idt_table[0x20].offset_h = (uint32_t)timer_int >> 16;
+    idt_table[0x20].selector = KERNEL_COOE_SEG; // 选择子选代码段
+    idt_table[0x20].attr = 0x8E00;              // 含义见intel编程文档
 
     pg_dir[MAP_ADDR >> 22] = (uint32_t)page_table | PDE_P | PDE_W | PDE_U;
     page_table[(MAP_ADDR >> 12) & 0x3FF] = (uint32_t)map_phy_buffer | PDE_P | PDE_W | PDE_U;
